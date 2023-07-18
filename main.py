96dcf1c3526026e02642a19d670afdb9f23eb232
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-07-18 19:54:29 +0200

    add info about strings and slices png

---
+++ main.py
@@ -0,0 +1,58 @@
+# print("lesha" in "My name is Lesha")
+
+          #    0123...
+# print(test_string.upper())
+# print(test_string.lower())
+# # TestEmail@test.com
+# # testemail@test.com
+#
+# print(test_string.startswith("My"))
+# print(test_string.startswith("name", 3))
+# print(test_string.endswith("Lesha"))
+
+# print(test_string_procent + "27")
+# print(test_string_procent + str(27))
+# test_string_procent = "My name is Lesha, my age is %s, %s I'm lived in Kharkiv"
+# procent_result = test_string_procent % (27, 30)
+# print(procent_result)
+
+
+test_string_format = "My name is Lesha, my age is {}, I'm lived in {}"
+# test_string_format_2 = "My name is Lesha, my age is {age}, I'm lived in {city}"
+#
+format_result = test_string_format.format(27, "Kharkiv")
+# format_result = test_string_format_2.format(age=27, city="Kharkiv")
+# format_result = 2
+# print(format_result)
+#
+# my_age = 27
+# test_string_fstring = f"My name is Lesha, my age is {my_age}, {'something'}, {True}"
+# # test_string_fstring = f"My nAme is Lesha, my age is {50 - 23}, {'something'}, {True}"
+# # print(test_string_fstring)
+#
+#
+# print(test_string_fstring.capitalize())
+# print(test_string_fstring.title())
+# print(test_string_fstring.count("my"))
+# print(test_string_fstring.count("my"))
+# string_length_example = len(test_string_fstring)
+# print(f'symbols count: {string_length_example}')
+# print(test_string_fstring.find('name', 4, 10))
+# int_str = "123 "
+# str_str = "asd "
+# alnum_str = "asd1"
+# print(int_str.isnumeric())
+# print(str_str.isalpha())
+# print(alnum_str.isalnum())
+
+     #        234567
+test_str = "123456789"
+# print(test_str[0])
+# print(test_str[5])
+# print(test_str[6])
+# print(test_str[0:6])
+# print(test_str[2:7:2])
+# print(test_str[-1])
+# print(test_str[-8:-1:2])
+# print(test_str[len(test_str) - 1])
+# print(test_str[-1])
