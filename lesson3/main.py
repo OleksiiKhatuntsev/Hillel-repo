0d667d3bef56eab8d31877ac064639a442b7d19f
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-07-25 18:33:25 +0200

    new_change_to_git

---
+++ lesson3/main.py
@@ -0,0 +1,6 @@
+# input_example = input("Enter name: ")
+# example_string = "Alex"
+# result = f"Word {input_example} has {len(input_example)} letters"
+# result_format = "Word {word} has {len_word} letters".format(word=input_example, len_word=len(input_example))
+# print(result_format)
+# print(f"Word {input('Enter name:')}, has {len(input('Enter name:'))} letters")
---
+++ main_1.py
@@ -0,0 +1,4 @@
+input_string = input()
+input_quotes = "'" + input_string + "'"
+number_of_letters = f"Word {input_quotes} has {len(input_string)} letters"
+print(number_of_letters)
